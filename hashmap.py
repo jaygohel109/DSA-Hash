785afbaa0cc6425649ab15d3bcd9489f7b6b547a
Gohel Jay <56113257+jaygohel109@users.noreply.github.com>
2025-02-13 10:44:43 -0600

    Create hashmap.py

---
+++ hashmap.py
@@ -0,0 +1,63 @@
+# Input and Output:
+# Input
+# ["MyHashMap", "put", "put", "get", "get", "put", "get", "remove", "get"]
+# [[], [1, 1], [2, 2], [1], [3], [2, 1], [2], [2], [2]]
+# Output
+# [null, null, null, 1, -1, null, 1, null, -1]
+
+# Explanation
+# MyHashMap myHashMap = new MyHashMap();
+# myHashMap.put(1, 1); // The map is now [[1,1]]
+# myHashMap.put(2, 2); // The map is now [[1,1], [2,2]]
+# myHashMap.get(1);    // return 1, The map is now [[1,1], [2,2]]
+# myHashMap.get(3);    // return -1 (i.e., not found), The map is now [[1,1], [2,2]]
+# myHashMap.put(2, 1); // The map is now [[1,1], [2,1]] (i.e., update the existing value)
+# myHashMap.get(2);    // return 1, The map is now [[1,1], [2,1]]
+# myHashMap.remove(2); // remove the mapping for 2, The map is now [[1,1]]
+# myHashMap.get(2);    // return -1 (i.e., not found), The map is now [[1,1]]
+
+class ListNode:
+    def __init__(self, key = -1, val = -1, next = None):
+        self.key = key
+        self.val = val
+        self.next = next
+
+class MyHashMap:
+    def __init__(self):
+        self.map = [ListNode() for i in range(1000)]
+    
+    def hash(self, key):
+        return key % len(self.map)
+
+    def put(self, key: int, value: int) -> None:
+        cur = self.map[self.hash(key)]
+        while cur.next:
+            if cur.next.key == key:
+                cur.next.val = value
+                return
+            cur = cur.next
+        cur.next = ListNode(key, value)
+
+    def get(self, key: int) -> int:
+        cur = self.map[self.hash(key)].next
+        while cur:
+            if cur.key == key:
+                return cur.val
+            cur = cur.next
+        return -1
+
+    def remove(self, key: int) -> None:
+        cur = self.map[self.hash(key)]
+        while cur and cur.next:
+            if cur.next.key == key:
+                cur.next = cur.next.next
+                return
+            cur = cur.next 
+        
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
